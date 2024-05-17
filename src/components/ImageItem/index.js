7c003e2082835110189ea47c95242424552f5e7f
Siva Dinesh .k <ksd.ccbp4@gmail.com>
2024-05-17 16:12:59 +0530

    tool tip added

--- src/components/ImageItem/index.js
+++ src/components/ImageItem/index.js
@@ -1,12 +1,25 @@
+import { Popup } from 'reactjs-popup'
+
 import './index.css'
+
+
 
 const ImageItem = props => {
 	const { imageDetails, altVal } = props
-	const { description, likes, imageUrl, updatedAt, uploadedBy } = imageDetails
+	const { description, likes, imageUrl, uploadedBy } = imageDetails
+	const position = 'top center'
 
 	return (
+
 		<li className="image-item">
-			<img src={imageUrl} alt={altVal} className='image' />
+			<Popup
+				trigger={open => (
+					<img src={imageUrl} alt={altVal} className='image' /> )} closeOnDocumentClick position='top center' on={[ 'hover', 'focus' ]} arrow={position !== 'center center'}>
+
+				<p>{description}</p>
+				<p>likes: {likes}</p>
+				<p>clicked by: {uploadedBy}</p>
+			</Popup >
 		</li>
 	)
 }
